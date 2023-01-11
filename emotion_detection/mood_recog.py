54b77edbe3079a8e0123c1fdf8a62a6cf08358d4
Anvi <anvipadiyar@gmail.com>
2023-01-11 18:34:31 -0500

    kv file

--- emotion_detection/mood_recog.py
+++ emotion_detection/mood_recog.py
@@ -8,127 +8,19 @@ from kivy.core.window import Window
 from kivy.uix.textinput import TextInput
 import time
 
-Builder.load_string('''
-<CameraScreen>:
-    BoxLayout:
-        orientation: 'vertical'
-        Camera:
-            id: camera
-            play: True
-        Button:
-            text: 'Capture'
-            on_press: root.capture()
-            background_normal: 'button.png'
+Builder.load_file('emotion_detection/kv_file.kv')
 
-<MenuScreen>:
-    BoxLayout:
-        orientation: 'vertical'
-        Label:
-            color: 0, 0, 0, 1
-            text: 'Emotion Detection'
-            size_hint: 0.4, 0.3
-            pos_hint: {'center_x':.5, 'center_y':.25}
-        Label:
-            color: 0, 0, 0, 1
-            text: 'Detect emotions and facial expressions in real-time with machine learning'
-            size_hint: 0.4, 0.3
-            pos_hint: {'center_x':.5, 'center_y':.5}
-        Button:
-            text: 'Login'
-            background_normal: 'button.png'
-            on_press: root.manager.current = 'login'
-            size_hint: 0.4, 0.2
-            pos_hint: {'center_x':.5, 'center_y':.7}
-        Button:
-            text: 'Signup'
-            background_normal: 'button.png'
-            on_press: root.manager.current = 'signup'
-            pos_hint: {'center_x':.5, 'center_y': .8}
-            size_hint: 0.4, 0.2
-<LoginScreen>:
-    BoxLayout:
-        orientation: 'vertical'
-        TextInput:
-            id: input_user
-            hint_text:'Enter username'
-            pos_hint: {'center_x': 0.5, 'center_y': 0.1}
-            multiline:False
-        TextInput:
-            id: input_pass
-            hint_text:'Enter password'
-            pos_hint: {'center_x': 0.5, 'center_y': 0.2}
-            multiline:False
-        Button:
-            text: 'Enter'
-            background_normal: 'button.png'
-            on_press: root.manager.current = 'home'
-            size_hint: 0.4, 0.2
-            pos_hint: {'center_x':.5, 'center_y':.7}
-<SignupScreen>:
-    BoxLayout:
-        orientation: 'vertical'
-        TextInput:
-            id: input_user
-            hint_text:'Enter username'
-            pos_hint: {'center_x': 0.5, 'center_y': 0.1}
-            multiline:False
-        TextInput:
-            id: input_pass
-            hint_text:'Enter password'
-            pos_hint: {'center_x': 0.5, 'center_y': 0.2}
-            multiline:False
-        Button:
-            text: 'Enter'
-            background_normal: 'button.png'
-            on_press: root.process()
-            size_hint: 0.4, 0.2
-            pos_hint: {'center_x':.5, 'center_y':.7}
-<HomeScreen>:
-    BoxLayout:
-        orientation: 'vertical'
-        Label:
-            color: 0, 0, 0, 1
-            text: 'Detect emotions and facial expressions in real-time with machine learning'
-            size_hint: 0.4, 0.3
-            pos_hint: {'center_x':.5, 'center_y':.3}
-        Button:
-            text: 'Camera'
-            background_normal: 'button.png'
-            on_press: root.manager.current = 'camera'
-            size_hint: 0.4, 0.2
-            pos_hint: {'center_x':.5, 'center_y':.7}
-        Button:
-            text: 'Gallery'
-            background_normal: 'button.png'
-            on_press: root.manager.current = 'gallery'
-            pos_hint: {'center_x':.5, 'center_y': .8}
-            size_hint: 0.4, 0.2
-        Button:
-            text: 'Profile'
-            background_normal: 'button.png'
-            on_press: root.manager.current = 'camera'
-            size_hint: 0.4, 0.2
-            pos_hint: {'center_x':.5, 'center_y':.9}
-        Button:
-            text: 'Stats'
-            background_normal: 'button.png'
-            on_press: root.manager.current = 'gallery'
-            pos_hint: {'center_x':.5, 'center_y': 1}
-            size_hint: 0.4, 0.2
-''')
+class MenuScreen(Screen):
+    pass
 
 
 class CameraScreen(Screen):
     def capture(self):
         camera = self.ids['camera']
         timestr = time.strftime("%Y%m%d_%H%M%S")
-        camera.export_to_png("user_images/IMG_" + timestr)
-        print("captured")
-
-
-class MenuScreen(Screen):
-    pass
-
+        camera.export_to_png("emotion_detection/user_images/IMG_{}.png".format(timestr))
+    def recognize_mood(self):
+        pass
 
 class LoginScreen(Screen):
     def process(self):
@@ -148,7 +40,6 @@ class SignupScreen(Screen):
 class HomeScreen(Screen):
     pass
 
-
 class MainApp(App):
     def build(self):
         Window.clearcolor = (1, 1, 1, 1)
@@ -157,9 +48,6 @@ class MainApp(App):
         sm.add_widget(LoginScreen(name='login'))
         sm.add_widget(SignupScreen(name='signup'))
         sm.add_widget(HomeScreen(name='home'))
-        sm.add_widget(CameraScreen(name='camera'))
+        sm.add_widget(CameraScreen(name='camera_page'))
         return sm
-
-
-if __name__ == '__main__':
-    MainApp().run()
+MainApp().run()
